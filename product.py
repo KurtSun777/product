27622b64193168b8a90be7d06a69e0da5043f31a
kurtsun <kurt.sun777@gmail.com>
2022-03-02 06:12:39 +0800

    continue

--- product.py
+++ product.py
@@ -1,24 +1,40 @@
 product = []
 
-while True:
-    name = input('please tell me which products you need: ')
-    if name == 'q':
-        break
-    
-    price = input('please enter the price: ')
-
-    #p = []
-    #p.append(name)
-    #p.append(price)
-
-    #p = [name, price]
-    #product.append(p)
-
-    product.append([name, price])
+with open ('product.csv', 'r', encoding = 'utf-8') as f:
+    for line in f:
+        if 'product, price' in line:
+            continue
+        name, price = line.strip().split(',')
+        product.append([name, price])
 print(product)
 
-print(product[0][0])
+# while True:
+#     name = input('please tell me which products you need: ')
+#     if name == 'q':
+#         break
+    
+#     price = input('please enter the price: ')
+#     price = int(price)
 
-for p in product:
-    print(p)
-    print(p[0])
+#     #p = []0
+
+#     #p.append(name)
+#     #p.append(price)
+
+#     #p = [name, price]
+#     #product.append(p)
+
+#     product.append([name, price])
+# print(product)
+
+# #print(product[0][0])
+
+# for p in product:
+#     print(p)
+#     #print(p[0])
+
+# with open ('product.csv', 'w', encoding = 'utf-8') as f:
+#     f.write('product, price' + '\n')
+#     for p in product:
+#         f.write(p[0] + ',' + str(p[1]) + '\n')
+
